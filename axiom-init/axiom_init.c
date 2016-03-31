39979a5ba9b6b5cb0f6caa42ef82f834b031d66b
Lisa <l.tani@evidence.com>
2016-03-31 10:20:06 +0200

    first axiom_init implementation [without qemu library]

---
+++ axiom-init/axiom_init.c
@@ -0,0 +1,57 @@
+/*
+ * axiom_discovery_protocol_test.c
+ *
+ * Version:     v0.2
+ * Last update: 2016-03-23
+ *
+ * This file tests the AXIOM INIT implementation
+ *
+ */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include "axiom_nic_types.h"
+#include "axiom_node_code.h"
+
+#define MASTER_PARAMETER  1
+
+int main(int argc, char **argv)
+{
+    int master_slave;
+    axiom_dev_t *dev;
+    axiom_node_id_t topology[AXIOM_NUM_NODES][AXIOM_NUM_INTERFACES];
+    axiom_if_id_t routing_tables[AXIOM_NUM_NODES][AXIOM_NUM_NODES];
+    axiom_if_id_t final_routing_table[AXIOM_NUM_NODES];
+
+    /* first parameter: Master or slave */
+    if (argc < 2) {
+        printf("Parameter required: Master or Slave\n");
+        exit(-1);
+    }
+
+    if (sscanf(argv[1], "%i", &master_slave) != 1) {
+        perror("parameter is not an integer");
+        exit(-1);
+    }
+
+    if (master_slave == MASTER_PARAMETER)
+    {
+        /* Master code */
+        axiom_master_node_code(dev, topology, routing_tables, final_routing_table);
+    }
+    else
+    {
+        /* Slave code */
+        axiom_slave_node_code(dev, topology, final_routing_table);
+    }
+
+    return 0;
+}
