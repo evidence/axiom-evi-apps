fb794af7bdee980a49cfbc821a9f425259d5105e
Massimiliano Giorgi <m.giorgi@evidence.eu.com>
2017-01-03 21:16:29 +0100

    structure renovation: -to remove extra projects location dependencies -to use/generate pkg-config files -to generate dynamic libraries

--- Makefile
+++ Makefile
@@ -1,61 +1,22 @@
-AXIOM_INCLUDE := ../axiom-evi-nic/include
-AXIOM_APPS_INCLUDE := ./include
-AXIOM_ALLOC_INCLUDE := ../axiom-allocator/include
-APPS_DIR := axiom-init axiom-recv axiom-send axiom-whoami axiom-info
-APPS_DIR += axiom-ping axiom-traceroute axiom-netperf axiom-rdma axiom-run axiom-utility
-LIBS_DIR := axiom_common_library
-CLEAN_DIR := $(addprefix _clean_, $(APPS_DIR) $(LIBS_DIR))
-INSTALL_DIR := $(addprefix _install_, $(APPS_DIR))
 
-PWD := $(shell pwd)
-#CCARCH := arm
-CCARCH := aarch64
-BUILDROOT := ${PWD}/../axiom-evi-buildroot
-DESTDIR := ${BUILDROOT}/output/target
-CCPREFIX := ${BUILDROOT}/output/host/usr/bin/$(CCARCH)-linux-
+APPS_DIR := axiom-init axiom-run axiom-recv axiom-send axiom-whoami 
+APPS_DIR += axiom-ping axiom-traceroute axiom-netperf axiom-rdma axiom-info
+APPS_DIR += axiom-utility
+LIBS_DIR := axiom-init axiom-run
+COMS_DIR := axiom_common_library
 
-ifndef AXIOMHOME
-    AXIOMHOME=$(realpath ${PWD}/..)
-endif
-ifndef HOST_DIR
-   HOST_DIR=${AXIOMHOME}/output
-endif
+.PHONY: all libs com_libs clean install clean distclean 
 
-DFLAGS := -g -DPDEBUG
-CFLAGS += -Wall $(DFLAGS) -I$(PWD)/$(AXIOM_INCLUDE) -I$(PWD)/$(AXIOM_APPS_INCLUDE)
-CFLAGS += -I$(PWD)/$(AXIOM_ALLOC_INCLUDE)
+export DFLAGS
 
-.PHONY: all clean install installhost $(APPS_DIR) $(LIBS_DIR) $(CLEAN_DIR) $(INSTALL_DIR)
+all: libs
+	for DIR in $(APPS_DIR); do $(MAKE) -C $$DIR; done
 
-all: $(LIBS_DIR) $(APPS_DIR)
+libs: com_libs
+	for DIR in $(LIBS_DIR); do $(MAKE) -C $$DIR libs; done
 
-libs: $(LIBS_DIR)
-	cd axiom-init && CFLAGS="$(CFLAGS)" make CCPREFIX=$(CCPREFIX) DFLAGS="$(DFLAGS)" libs
-	cd axiom-run && CFLAGS="$(CFLAGS)" make CCPREFIX=$(CCPREFIX) DFLAGS="$(DFLAGS)" libs
+com_libs: 
+	for DIR in $(COMS_DIR); do $(MAKE) -C $$DIR; done
 
-$(APPS_DIR):: $(LIBS_DIR)
-
-axiom-run:: axiom-init
-
-$(LIBS_DIR) $(APPS_DIR)::
-	cd $@ && CFLAGS="$(CFLAGS)" make CCPREFIX=$(CCPREFIX) DFLAGS="$(DFLAGS)"
-
-install: all $(INSTALL_DIR)
-
-$(INSTALL_DIR):
-	cd $(subst _install_,,$@) &&  CFLAGS="$(CFLAGS)" make install CCPREFIX=$(CCPREFIX) DESTDIR=$(DESTDIR) DFLAGS="$(DFLAGS)"
-
-
-clean: $(CLEAN_DIR)
-
-$(CLEAN_DIR): _clean_%:
-	cd $(subst _clean_,,$@) && make clean
-
-
-installhost:
-	mkdir -p ${HOST_DIR}
-	cp -r include ${HOST_DIR}
-	mkdir -p ${HOST_DIR}/lib
-	cp axiom-init/lib/libaxiom_init_api.a ${HOST_DIR}/lib
-	cp axiom-run/lib/libaxiom_run_api.a ${HOST_DIR}/lib
-	cp axiom_common_library/libaxiom_common.a ${HOST_DIR}/lib
+install clean distclean:
+	for DIR in $(APPS_DIR) $(COMS_DIR); do $(MAKE) -C $$DIR $@; done
---
+++ common.mk
@@ -0,0 +1,83 @@
+
+#
+# default architecture
+#
+
+CCARCH := aarch64
+
+#
+# version numbers
+#
+
+VERSION := $(shell git describe --tags --dirty | sed -re 's/^axiom-v([0-9.]*).*/\1/')
+MAJOR := $(shell echo $(VERSION) | sed -e 's/\..*//')
+MINOR := $(shell echo $(VERSION) | sed -re 's/[0-9]*\.([0-9]*).*/\1/')
+SUBLEVEL := $(shell echo $(VERSION) | sed -r -e 's/([0-9]*\.){2}([0-9]*).*/\2/' -e 's/[0-9]*\.[0-9]*/0/')
+VERSION := $(MAJOR).$(MINOR).$(SUBLEVEL)
+
+#
+# variuos output directories
+#
+
+COMMKFILE_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+#BUILDROOT := ${COMMKFILE_DIR}/../axiom-evi-buildroot
+OUTPUT_DIR := ${COMMKFILE_DIR}/../output
+TARGET_DIR := $(realpath ${OUTPUT_DIR}/target)
+SYSROOT_DIR := $(realpath ${OUTPUT_DIR}/staging)
+HOST_DIR := $(realpath ${OUTPUT_DIR}/host)
+
+#
+# pkg-config configuration
+#
+
+#PKG-CONFIG_SILENCE:=--silence-errors
+
+PKG-CONFIG := PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1 PKG_CONFIG_ALLOW_SYSTEM_LIBS=1 PKG_CONFIG_DIR= PKG_CONFIG_LIBDIR=$(SYSROOT_DIR)/usr/lib/pkgconfig:$(SYSROOT_DIR)/usr/share/pkgconfig PKG_CONFIG_SYSROOT_DIR=$(SYSROOT_DIR) pkg-config $(PKG-CONFIG_SILENCE)
+ifeq ($(shell $(PKG-CONFIG) axiom_user_api >/dev/null || echo fail),fail)
+    # GNU make bug 10593: an environment variale can not be inject into sub-shell (only in sub-make)
+    # so a default for PKG_CONFIG_PATH can not be set into a makefile (and used by sub-shell)
+    $(warning axiom_user_api.pc is not found (PKG_CONFIG_SYSROOT_DIR is '$(SYSROOT_DIR)' $(TARGET_DIR)))
+endif
+
+# PKG-CFLAGS equivalent to "pkg-config --cflags ${1}" into the cross sysroot
+PKG-CFLAGS = $(shell $(PKG-CONFIG) --cflags ${1})
+# PKG-LDFLAGS equivalent to "pkg-config --libs-only-other --libs-only-L ${1}" into the cross sysroot
+PKG-LDFLAGS = $(shell $(PKG-CONFIG) --libs-only-other --libs-only-L ${1})
+# PKG-LDLIBS equvalent to "pkg-config --libs-only-l ${1}" into the cross sysroot
+PKG-LDLIBS = $(shell $(PKG-CONFIG) --libs-only-l ${1})
+
+#
+# internal directory structure
+#
+
+AXIOM_APPS_INCLUDE := $(COMMKFILE_DIR)/include
+
+AXIOM_COMMON_CFLAGS := -I$(COMMKFILE_DIR)/axiom_common_library
+AXIOM_COMMON_LDFLAGS := -L$(COMMKFILE_DIR)/axiom_common_library
+AXIOM_COMMON_LDLIBS := -laxiom_common
+
+#
+# tools
+#
+
+CCPREFIX := ${HOST_DIR}/usr/bin/$(CCARCH)-linux-
+CC := ${CCPREFIX}gcc
+AR := ${CCPREFIX}ar
+RANLIB := ${CCPREFIX}ranlib
+
+#DFLAGS := -g -DPDEBUG
+DFLAGS := -g
+
+#
+# source file dependencies management
+#
+
+DEPFLAGS = -MT $@ -MMD -MP -MF $*.Td
+
+%.o: %.c
+%.o: %.c %.d
+	$(CC) $(DEPFLAGS) $(CPPFLAGS) $(CFLAGS) -c -o $@ $<
+	mv -f $*.Td $*.d
+
+%.d: ;
+.PRECIOUS: %.d
