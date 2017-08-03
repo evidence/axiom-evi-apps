f8be290d112a13c3076bc3227e918c6f7683b23b
Massimiliano Giorgi <m.giorgi@evidence.eu.com>
2017-08-03 09:55:34 +0000

    add support for X86 native compilation (on ubuntu xenial)

--- common.mk
+++ common.mk
@@ -3,7 +3,11 @@
 # default architecture
 #
 
+ifeq ($(FS),x86)
+undefine CCARCH
+else
 CCARCH := aarch64
+endif
 
 #
 # version numbers
@@ -26,10 +30,16 @@ TARGET_DIR := $(realpath ${ROOTFS})
 SYSROOT_DIR := $(realpath ${ROOTFS})
 HOST_DIR := $(realpath ${LINARO}/host)
 else
+ifeq ($(FS),x86)
+TARGET_DIR := /
+SYSROOT_DIR := /
+HOST_DIR := /
+else
 OUTPUT_DIR := ${COMMKFILE_DIR}/../output
 TARGET_DIR := $(realpath ${OUTPUT_DIR}/target)
 SYSROOT_DIR := $(realpath ${OUTPUT_DIR}/staging)
 HOST_DIR := $(realpath ${OUTPUT_DIR}/host)
+endif
 endif
 
 SYSROOT_INST_DIR ?= $(SYSROOT_DIR)
@@ -50,7 +60,12 @@ endif
 
 #PKG-CONFIG_SILENCE:=--silence-errors
 
+ifeq ($(FS),x86)
+PKG-CONFIG := PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1 PKG_CONFIG_ALLOW_SYSTEM_LIBS=1 PKG_CONFIG_DIR= PKG_CONFIG_LIBDIR=$(SYSROOT_DIR)/usr/lib/pkgconfig:$(SYSROOT_DIR)/usr/local/lib/pkgconfig:$(SYSROOT_DIR)/usr/share/pkgconfig PKG_CONFIG_SYSROOT_DIR=$(SYSROOT_DIR) pkg-config $(PKG-CONFIG_SILENCE)
+else
 PKG-CONFIG := PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1 PKG_CONFIG_ALLOW_SYSTEM_LIBS=1 PKG_CONFIG_DIR= PKG_CONFIG_LIBDIR=$(SYSROOT_DIR)/usr/lib/pkgconfig:$(SYSROOT_DIR)/usr/share/pkgconfig PKG_CONFIG_SYSROOT_DIR=$(SYSROOT_DIR) pkg-config $(PKG-CONFIG_SILENCE)
+endif
+
 ifeq ($(shell $(PKG-CONFIG) axiom_user_api >/dev/null || echo fail),fail)
     # GNU make bug 10593: an environment variale can not be inject into sub-shell (only in sub-make)
     # so a default for PKG_CONFIG_PATH can not be set into a makefile (and used by sub-shell)
@@ -81,7 +96,11 @@ AXIOM_COMMON_LDLIBS := -laxiom_common
 ifeq ($(FS),seco)
 CCPREFIX := ${LINARO}/host/usr/bin/$(CCARCH)-linux-gnu-
 else
+ifeq ($(FS),br)
 CCPREFIX := ${HOST_DIR}/usr/bin/$(CCARCH)-linux-
+else
+CCPREFIX :=
+endif
 endif
 CC := ${CCPREFIX}gcc
 AR := ${CCPREFIX}ar
